641704aca37c95c5bf0c574b0ff30a166b548eb0
Rochonastore <58441054+rochonastore@users.noreply.github.com>
2020-05-05 20:41:47 +0600

    Create visitortracking.js

---
+++ visitortracking.js
@@ -0,0 +1,11 @@
+var txt = "";
+	txt += "Url: " + event.data; + "\n";
+	txt += "Browser CodeName: " + navigator.appCodeName + "\n";
+	txt += "Browser Name: " + navigator.appName + "\n";
+//	txt += "Browser Version: " + navigator.appVersion + "\n";
+//	txt += "Cookies Enabled: " + navigator.cookieEnabled + "\n";
+//	txt += "Browser Language: " + navigator.language + "\n";
+//	txt += "Browser Online: " + navigator.onLine + "\n";
+	txt += "Platform: " + navigator.platform + "\n";
+	txt += "User-agent header: " + navigator.userAgent + "\n";
+	txt += "Ip Address: " + userip + "\n";
