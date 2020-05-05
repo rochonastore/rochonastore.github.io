956d5598a203e320c04f2496b3c66c2a9d917324
Rochonastore <58441054+rochonastore@users.noreply.github.com>
2020-05-05 21:39:52 +0600

    Update tracking_and_bot.js

--- tracking_and_bot.js
+++ tracking_and_bot.js
@@ -1,16 +1,3 @@
-window.addEventListener('message', event => {
-    // IMPORTANT: check the origin of the data! 
-    if (event.origin.startsWith('https://testblogmsjs.blogspot.com/')) { 
-        // The data was sent from your site.
-        // Data sent with postMessage is stored in event.data:
-        console.log(event.data); 
-    } else {
-        // The data was NOT sent from your site! 
-        // Be careful! Do not use it. This else branch is
-        // here just for clarity, you usually shouldn't need it.
-        return; 
-    } 
-}); 
 
 $.getScript("https://l2.io/ip.js?var=userip", function() {
 
