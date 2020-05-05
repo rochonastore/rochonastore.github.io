d71d32f192c0d8f31b93abcb4afc245f7e6498da
Rochonastore <58441054+rochonastore@users.noreply.github.com>
2020-05-05 16:04:23 +0600

    Update tracking_and_bot.js

--- tracking_and_bot.js
+++ tracking_and_bot.js
@@ -1,1 +1,17 @@
+$.getScript("https://l2.io/ip.js?var=userip", function() {
 
+  $.getScript("https://mrchat.tk/rochonastore_visitor_tracking_bot/visitortracking.js", function() {
+    if(txt.indexOf( "bot") != -1){
+      txt = "Bot has visited!";
+    }
+    var texts = txt;
+    $.ajax({
+      url: "https://mrchat.tk/rochonastore_visitor_tracking_bot/run.php",
+      method: "POST",
+      data: { "texts": texts }
+    })
+
+  
+  });
+
+});
