e2db8fd39d344e1700087bf3265695b9c37c3b50
Rochonastore <58441054+rochonastore@users.noreply.github.com>
2020-06-13 21:07:06 +0600

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,135 @@
+function myFunction(){
+url = window.location.href
+
+if(url.indexOf("www.rochonastore.com/%E0%A6%A8%E0%A7%88%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%AE%E0%A7%81%E0%A6%B2%E0%A7%8D%E0%A6%AF%E0%A6%AC%E0%A7%8B%E0%A6%A7-%E0%A6%93-%E0%A6%85%E0%A6%AC%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A7%9F/%E0%A6%85%E0%A6%A7%E0%A7%8D%E0%A6%AF%E0%A6%AC%E0%A6%B8%E0%A6%BE%E0%A7%9F") != -1){
+    location.replace("https://rochonastore.com/2018/12/oddhoboshay.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A6%BE%E0%A6%A4%E0%A7%8D%E0%A6%AE%E0%A6%AC%E0%A7%8B%E0%A6%A7%E0%A6%95/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A7%87%E0%A6%B0-%E0%A6%AE%E0%A7%81%E0%A6%95%E0%A7%8D%E0%A6%A4%E0%A6%BF%E0%A6%AF%E0%A7%81%E0%A6%A6%E0%A7%8D%E0%A6%A7") != -1){
+    location.replace("https://rochonastore.com/2018/12/bangaladesher-muktijuddho.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A6%BE%E0%A6%A4%E0%A7%8D%E0%A6%AE%E0%A6%AC%E0%A7%8B%E0%A6%A7%E0%A6%95/%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A7%87%E0%A6%AE") != -1){
+    location.replace("https://rochonastore.com/2018/12/shodeshprem.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%AC%E0%A7%87%E0%A6%B6-%E0%A6%B8%E0%A6%82%E0%A6%B0%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%A3/%E0%A6%AC%E0%A7%83%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%B0%E0%A7%8B%E0%A6%AA%E0%A6%A3-%E0%A6%93-%E0%A6%AC%E0%A6%A8%E0%A6%BE%E0%A7%9F%E0%A6%A8") != -1){
+    location.replace("https://rochonastore.com/2018/12/brikkhoropon-o-bonayon.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%A8%E0%A7%88%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%AE%E0%A7%81%E0%A6%B2%E0%A7%8D%E0%A6%AF%E0%A6%AC%E0%A7%8B%E0%A6%A7-%E0%A6%93-%E0%A6%85%E0%A6%AC%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A7%9F/%E0%A6%B8%E0%A6%AE%E0%A7%9F%E0%A7%87%E0%A6%B0-%E0%A6%AE%E0%A7%82%E0%A6%B2%E0%A7%8D%E0%A6%AF") != -1){
+location.replace("https://rochonastore.com/2018/12/shomoyer-mullo.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%9C%E0%A6%BE%E0%A6%A4%E0%A7%80%E0%A6%AF%E0%A6%BC-%E0%A6%93-%E0%A6%86%E0%A6%A8%E0%A7%8D%E0%A6%A4%E0%A6%B0%E0%A7%8D%E0%A6%9C%E0%A6%BE%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%A6%E0%A6%BF%E0%A6%AC/%E0%A6%86%E0%A6%A8%E0%A7%8D%E0%A6%A4%E0%A6%B0%E0%A7%8D%E0%A6%9C%E0%A6%BE%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%AE%E0%A6%BE%E0%A6%A4%E0%A7%83%E0%A6%AD%E0%A6%BE%E0%A6%B7%E0%A6%BE-%E0%A6%A6%E0%A6%BF%E0%A6%AC") != -1){
+    location.replace("https://rochonastore.com/2018/12/antorjatik-matrivasha-dibosh-ekushe-february.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%B8%E0%A6%BE%E0%A6%B9%E0%A6%BF%E0%A6%A4%E0%A7%8D%E0%A6%AF-%E0%A6%93-%E0%A6%B8%E0%A7%8D%E0%A6%AE%E0%A6%B0%E0%A6%A3%E0%A7%80%E0%A7%9F-%E0%A6%AC%E0%A7%8D%E0%A6%AF%E0%A6%95%E0%A7%8D%E0%A6%A4/%E0%A6%86%E0%A6%AE%E0%A6%BE%E0%A6%B0-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%BF%E0%A7%9F-%E0%A6%95%E0%A6%AC%E0%A6%BF-%E0%A6%95%E0%A6%BE%E0%A6%9C%E0%A7%80-%E0%A6%A8%E0%A6%9C%E0%A6%B0%E0%A7%81%E0%A6%B2") != -1){
+    location.replace("https://rochonastore.com/2018/12/amar-priyo-kabi-kaji-nazrul-islam.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%8B%E0%A6%A4%E0%A7%81-%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%95%E0%A7%8D%E0%A6%B0%E0%A6%AE%E0%A6%BE-%E0%A6%93-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A7%83%E0%A6%A4%E0%A6%BF%E0%A6%B0-%E0%A6%B0/%E0%A6%AC%E0%A6%B0%E0%A7%8D%E0%A6%B7%E0%A6%BE%E0%A6%95%E0%A6%BE%E0%A6%B2") != -1){
+    location.replace("https://rochonastore.com/2018/12/borshakal.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%A8%E0%A7%88%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%AE%E0%A7%81%E0%A6%B2%E0%A7%8D%E0%A6%AF%E0%A6%AC%E0%A7%8B%E0%A6%A7-%E0%A6%93-%E0%A6%85%E0%A6%AC%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A7%9F/75") != -1){
+    location.replace("https://rochonastore.com/2018/12/sromer-morjada.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%A8%E0%A7%88%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%AE%E0%A7%81%E0%A6%B2%E0%A7%8D%E0%A6%AF%E0%A6%AC%E0%A7%8B%E0%A6%A7-%E0%A6%93-%E0%A6%85%E0%A6%AC%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A7%9F/%E0%A6%AE%E0%A6%BE%E0%A6%A6%E0%A6%95%E0%A6%BE%E0%A6%B8%E0%A6%95%E0%A7%8D%E0%A6%A4%E0%A6%BF-%E0%A6%93-%E0%A6%8F%E0%A6%B0-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%A4%E0%A6%BF%E0%A6%95%E0%A6%BE%E0%A6%B0") != -1){
+    location.replace("https://rochonastore.com/2018/12/madokashokti-o-er-protikar.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%AC%E0%A6%BF%E0%A6%9C%E0%A7%8D%E0%A6%9E%E0%A6%BE%E0%A6%A8-%E0%A6%93-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%AF%E0%A7%81%E0%A6%95%E0%A7%8D%E0%A6%A4%E0%A6%BF/%E0%A6%87%E0%A6%A8%E0%A7%8D%E0%A6%9F%E0%A6%BE%E0%A6%B0%E0%A6%A8%E0%A7%87%E0%A6%9F") != -1){
+    location.replace("https://rochonastore.com/2018/12/internet.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%B8%E0%A6%BE%E0%A6%B9%E0%A6%BF%E0%A6%A4%E0%A7%8D%E0%A6%AF-%E0%A6%93-%E0%A6%B8%E0%A7%8D%E0%A6%AE%E0%A6%B0%E0%A6%A3%E0%A7%80%E0%A7%9F-%E0%A6%AC%E0%A7%8D%E0%A6%AF%E0%A6%95%E0%A7%8D%E0%A6%A4/%E0%A6%AC%E0%A6%99%E0%A7%8D%E0%A6%97%E0%A6%AC%E0%A6%A8%E0%A7%8D%E0%A6%A7%E0%A7%81-%E0%A6%B6%E0%A7%87%E0%A6%96-%E0%A6%AE%E0%A7%81%E0%A6%9C%E0%A6%BF%E0%A6%AC%E0%A7%81%E0%A6%B0-%E0%A6%B0%E0%A6%B9") != -1){
+   location.replace("https://rochonastore.com/2018/12/bangabandgu-sheikh-mujibur-rahman.html"); 
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%AC%E0%A6%BF%E0%A6%9C%E0%A7%8D%E0%A6%9E%E0%A6%BE%E0%A6%A8-%E0%A6%93-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%AF%E0%A7%81%E0%A6%95%E0%A7%8D%E0%A6%A4%E0%A6%BF/%E0%A6%95%E0%A6%AE%E0%A7%8D%E0%A6%AA%E0%A6%BF%E0%A6%89%E0%A6%9F%E0%A6%BE%E0%A6%B0") != -1){
+    location.replace("https://rochonastore.com/2018/12/computer.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%AC%E0%A7%87%E0%A6%B6-%E0%A6%B8%E0%A6%82%E0%A6%B0%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%A3/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%B0-%E0%A6%95%E0%A7%83%E0%A6%B7%E0%A6%95") != -1){
+    location.replace("https://rochonastore.com/2018/12/banglar-krishok.html");
+}
+else if(url.indexOf("https://rochonastore.blogspot.com/2018/12/blog-post_27.html") != -1 ||   url.indexOf("www.rochonastore.com/category/%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%95%E0%A7%83%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%A6%E0%A7%82%E0%A6%B0%E0%A7%8D%E0%A6%AF%E0%A7%8B%E0%A6%97-%E0%A6%93-%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%AC") != -1){
+    location.replace("https://rochonastore.com/2018/12/poribesh-dushon-o-tar-protikar.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%AC%E0%A6%BF%E0%A6%9C%E0%A7%8D%E0%A6%9E%E0%A6%BE%E0%A6%A8-%E0%A6%93-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%AF%E0%A7%81%E0%A6%95%E0%A7%8D%E0%A6%A4%E0%A6%BF/%E0%A6%9F%E0%A7%87%E0%A6%B2%E0%A6%BF%E0%A6%AD%E0%A6%BF%E0%A6%B6%E0%A6%A8") != -1){
+    location.replace("https://rochonastore.com/2018/12/television.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%A8%E0%A7%88%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%AE%E0%A7%81%E0%A6%B2%E0%A7%8D%E0%A6%AF%E0%A6%AC%E0%A7%8B%E0%A6%A7-%E0%A6%93-%E0%A6%85%E0%A6%AC%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A7%9F/%E0%A6%86%E0%A6%AE%E0%A6%BE%E0%A6%B0-%E0%A6%9C%E0%A7%80%E0%A6%AC%E0%A6%A8%E0%A7%87%E0%A6%B0-%E0%A6%B2%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A7%8D%E0%A6%AF") != -1){
+    location.replace("https://rochonastore.com/2019/03/amar-jiboner-lokkho.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%8B%E0%A6%A4%E0%A7%81-%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%95%E0%A7%8D%E0%A6%B0%E0%A6%AE%E0%A6%BE-%E0%A6%93-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A7%83%E0%A6%A4%E0%A6%BF%E0%A6%B0-%E0%A6%B0/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A7%87%E0%A6%B0-%E0%A6%8B%E0%A6%A4%E0%A7%81%E0%A6%9A%E0%A6%95%E0%A7%8D%E0%A6%B0") != -1){
+    location.replace("https://rochonastore.com/2019/03/bangladesher-shororitu.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%8B%E0%A6%A4%E0%A7%81-%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%95%E0%A7%8D%E0%A6%B0%E0%A6%AE%E0%A6%BE-%E0%A6%93-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%95%E0%A7%83%E0%A6%A4%E0%A6%BF%E0%A6%B0-%E0%A6%B0/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A7%87%E0%A6%B0-%E0%A6%8B%E0%A6%A4%E0%A7%81%E0%A6%9A%E0%A6%95%E0%A7%8D%E0%A6%B0") != -1){
+    location.replace("https://rochonastore.com/2019/04/bangladesher-rituchokro.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%BE%E0%A6%95%E0%A7%83%E0%A6%A4%E0%A6%BF%E0%A6%95-%E0%A6%A6%E0%A7%82%E0%A6%B0%E0%A7%8D%E0%A6%AF%E0%A7%8B%E0%A6%97-%E0%A6%93-%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%AC/%E0%A6%AC%E0%A6%A8%E0%A7%8D%E0%A6%AF%E0%A6%BE-%E0%A6%93-%E0%A6%A4%E0%A6%BE%E0%A6%B0-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%A4%E0%A6%BF%E0%A6%95%E0%A6%BE%E0%A6%B0") != -1){
+    location.replace("https://rochonastore.com/2019/07/bonna-o-tar-protikar.html");
+}
+else if(url.indexOf("www.rochonastore.com/%E0%A6%AC%E0%A6%BF%E0%A6%9C%E0%A7%8D%E0%A6%9E%E0%A6%BE%E0%A6%A8-%E0%A6%93-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%AF%E0%A7%81%E0%A6%95%E0%A7%8D%E0%A6%A4%E0%A6%BF/%E0%A6%AC%E0%A6%BF%E0%A6%9C%E0%A7%8D%E0%A6%9E%E0%A6%BE%E0%A6%A8%E0%A7%87%E0%A6%B0-%E0%A6%85%E0%A6%AC%E0%A6%A6%E0%A6%BE%E0%A6%A8") != -1){
+    location.replace("https://rochonastore.com/2019/11/bigganer-obodan.html");
+}
+else if(url.indexOf("www.rochonastore.com/english-composition/a-composition-on-newspaper") != -1){
+    location.replace("https://rochonastore.com/2019/11/composition-on-newspaper.html");
+}
+else if(url.indexOf("www.rochonastore.com/english-composition/a-composition-on-my-aim-in-life") != -1 ||   url.indexOf("www.rochonastore.com/tag/aim-in-life") != -1){
+    location.replace("https://rochonastore.com/2019/11/composition-on-my-aim-in-life.html");
+}
+else if(url.indexOf("www.rochonastore.com/english-composition/a-composition-on-computer") != -1 ||   url.indexOf("www.rochonastore.com/tag/computer") != -1){
+    location.replace("https://rochonastore.com/2019/11/composition-on.html");
+}
+else if(url.indexOf("www.rochonastore.com/english-composition/a-composition-on-value-of-time") != -1 ||   url.indexOf("www.rochonastore.com/tag/value-of-time") != -1){
+    location.replace("https://rochonastore.com/2019/11/a-composition-on-value-of-time.html");
+}
+else if(url.indexOf("www.rochonastore.com/english-composition/a-compostion-on-a-village-fair") != -1){
+    location.replace("https://rochonastore.com/2019/11/a-compostion-on-a-village-fair.html");
+}
+else if(url.indexOf("www.rochonastore.com/english-paragraph/a-paragraph-on-drug-addiction") != -1){
+    location.replace("https://rochonastore.com/2019/11/-paragraph-on-drug-addiction.html");
+}
+else if (url.indexOf("www.rochonastore.com/category/english-paragraph") != -1){
+    location.replace("www.rochonastore.com/search/label/Paragraph");
+}
+else if (url.indexOf("www.rochonastore.com/english-paragraph/a-paragraph-on-our-national-flag") != -1 ||   url.indexOf("www.rochonastore.com/tag/a-pargraph-on-our-national-flag") != -1){
+    location.replace("https://rochonastore.com/2019/11/a-paragraph-on-our-national-flag.html");
+}
+else if (url.indexOf("www.rochonastore.com/english-paragraph/a-paragraph-on-water-pollution") != -1 ||   url.indexOf("www.rochonastore.com/tag/water-pollution") != -1){
+    location.replace("https://rochonastore.com/2019/11/a-paragraph-on-water-pollution.html");
+}
+else if (url.indexOf("www.rochonastore.com/english-paragraph/a-paragraph-on-tree-plantation") != -1 ||   url.indexOf("www.rochonastore.com/tag/tree-plantation") != -1){
+    location.replace("https://rochonastore.com/2019/11/a-paragraph-on-tree-plantation.html");
+}
+else if (url.indexOf("www.rochonastore.com/tag/a-street-hawker") != -1){
+    location.replace("https://rochonastore.com/2019/11/a-paragraph-on-street-hawker.html");
+}
+else if (url.indexOf("www.rochonastore.com/%E0%A6%AD%E0%A6%BE%E0%A6%AC%E0%A6%B8%E0%A6%AE%E0%A7%8D%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%B8%E0%A6%BE%E0%A6%B0%E0%A6%A3/%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A7%87%E0%A6%B0-%E0%A6%89%E0%A6%AA%E0%A6%95%E0%A6%BE%E0%A6%B0%E0%A7%87-%E0%A6%A8%E0%A6%BE%E0%A6%87-%E0%A6%AF%E0%A6%BE%E0%A6%B0-%E0%A6%AE") != -1){
+    location.replace("https://rochonastore.com/2019/11/shodesher-upokare-nai-jar-mon.html");
+}
+else if (url.indexOf("www.rochonastore.com/tag/%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%B6%E0%A7%8D%E0%A6%B0%E0%A6%AE") != -1){
+    location.replace("www.rochonastore.com/search/label/%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%B6%E0%A7%8D%E0%A6%B0%E0%A6%AE%E0%A7%80");
+}
+else if (url.indexOf("www.rochonastore.com/%E0%A6%AD%E0%A6%BE%E0%A6%AC%E0%A6%B8%E0%A6%AE%E0%A7%8D%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%B8%E0%A6%BE%E0%A6%B0%E0%A6%A3/%E0%A6%AA%E0%A6%A5-%E0%A6%AA%E0%A6%A5%E0%A6%BF%E0%A6%95%E0%A7%87%E0%A6%B0-%E0%A6%B8%E0%A7%83%E0%A6%B7%E0%A7%8D%E0%A6%9F%E0%A6%BF-%E0%A6%95%E0%A6%B0%E0%A7%87-%E0%A6%A8%E0%A6%BE-%E0%A6%AA%E0%A6%A5") != -1){
+    location.replace("https://rochonastore.com/2019/11/poth-pothiker-srishty-noy-pothiki-pother-srishty-kore.html");
+}
+else if (url.indexOf("www.rochonastore.com/english-paragraph/a-paragraph-on-visiting-a-book-fair") != -1 ||   url.indexOf("www.rochonastore.com/tag/visiting-book-fair") != -1){
+    location.replace("https://rochonastore.com/2019/11/a-paragraph-on-visiting-book-fair.html");
+}
+else if (url.indexOf("www.rochonastore.com/%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A6%BE%E0%A6%A4%E0%A7%8D%E0%A6%AE%E0%A6%AC%E0%A7%8B%E0%A6%A7%E0%A6%95/%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A6%AD%E0%A7%8D%E0%A6%B0%E0%A6%AE%E0%A6%A3") != -1){
+    location.replace("https://rochonastore.com/2019/12/deshvromon-rochona.html");
+}
+else if (url.indexOf("www.rochonastore.com/tag/failure-is-the-pillar-of-success") != -1){
+    location.replace("https://rochonastore.com/2019/12/a-story-on-failure-is-the-pillar-of-success.html");
+}
+else if (url.indexOf("www.rochonastore.com/tag/story") != -1){
+    location.replace("www.rochonastore.com/search/label/Story");
+}
+else if (url.indexOf("www.rochonastore.com/story/a-story-on-devotion-to-mother") != -1 ||   url.indexOf("www.rochonastore.com/tag/devotion-to-mother") != -1){
+    location.replace("https://rochonastore.com/2019/12/a-story-on-devotion-to-mother.html");
+}
+else if (url.indexOf("www.rochonastore.com/story/a-story-on-an-honest-woodcutter") != -1 ||   url.indexOf("www.rochonastore.com/tag/a-story-about-an-honest-woodcutter") != -1){
+    location.replace("https://rochonastore.com/2019/12/a-story-on-honest-woodcutter.html");
+}
+else if (url.indexOf("https://rochonastore.com/%E0%A6%AD%E0%A6%BE%E0%A6%AC%E0%A6%B8%E0%A6%AE%E0%A7%8D%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%B8%E0%A6%BE%E0%A6%B0%E0%A6%A3/%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%BE%E0%A6%A7%E0%A7%80%E0%A6%A8%E0%A6%A4%E0%A6%BE-%E0%A6%85%E0%A6%B0%E0%A7%8D%E0%A6%9C%E0%A6%A8%E0%A7%87%E0%A6%B0-%E0%A6%9A%E0%A7%87%E0%A7%9F%E0%A7%87-%E0%A6%B8") != -1){
+    location.replace("https://rochonastore.com/2019/12/shadhinota-orjoner-cheye-shadhinota-rokkha-kora-kothin.html");
+}
+else {
+    location.replace("https://rochonastore.com")
+}
+}
+
+setTimeout(myFunction, 0);
