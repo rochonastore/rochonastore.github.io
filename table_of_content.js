879ba5f505173b3fe8f1af8855b5ed6930849e50
Rochonastore <58441054+rochonastore@users.noreply.github.com>
2020-07-09 01:39:10 +0600

    Update table_of_content.js

--- table_of_content.js
+++ table_of_content.js
@@ -1,24 +1,28 @@
-function mbtTOC() {
-    var mbtTOC = 0;
-    var i = 0;
-    var headlength = 0;
-    var gethead = 0;
-    headlength = document.getElementById("post-toc").getElementsByTagName("h4").length;
-    for (i = 0; i < headlength; i++) {
-        gethead = document.getElementById("post-toc").getElementsByTagName("h4")[i].textContent;
-        document.getElementById("post-toc").getElementsByTagName("h4")[i].setAttribute("id", "point" + i);
-        mbtTOC = "<li><a href='#point" + i + "'>" + gethead + "</a></li>";
-        document.getElementById("mbtTOC") = document.getElementById("mbtTOC") + mbtTOC;
+const button = document.getElementById('hello');
+button.addEventListener('click', () => {
+    
+    function mbtTOC() {
+        var mbtTOC = 0;
+        var i = 0;
+        var headlength = 0;
+        var gethead = 0;
+        headlength = document.getElementById("post-toc").getElementsByTagName("h4").length;
+        for (i = 0; i < headlength; i++) {
+            gethead = document.getElementById("post-toc").getElementsByTagName("h4")[i].textContent;
+            document.getElementById("post-toc").getElementsByTagName("h4")[i].setAttribute("id", "point" + i);
+            mbtTOC = "<li><a href='#point" + i + "'>" + gethead + "</a></li>";
+            document.getElementById("mbtTOC") = document.getElementById("mbtTOC") + mbtTOC;
+        }
     }
-}
 
-function mbtToggle() {
-    var mbt = document.getElementById('mbtTOC');
-    if (mbt.style.display === 'none') {
-        mbt.style.display = 'block';
-    } else {
-        mbt.style.display = 'none';
+    function mbtToggle() {
+        var mbt = document.getElementById('mbtTOC');
+        if (mbt.style.display === 'none') {
+            mbt.style.display = 'block';
+        } else {
+            mbt.style.display = 'none';
+        }
     }
-}
 
-mbtTOC();
+    mbtTOC();
+}
