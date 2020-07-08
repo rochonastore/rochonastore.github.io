65b441c7294c1683e5c87de83c8f61049055b283
Rochonastore <58441054+rochonastore@users.noreply.github.com>
2020-07-09 03:38:47 +0600

    Update table_of_content.js

--- table_of_content.js
+++ table_of_content.js
@@ -1,33 +1,57 @@
+function mbtTOC() {
+    var mbtTOC = 0;
+    var i = 0;
+    var headlength = 0;
+    var gethead = 0;
+    var tempTOC = 0;
+    
+    
+    if(document.getElementById("mbtTOCc").style.display == 'block'){
+        document.getElementById("mbtTOCc").style.display = 'none';
+    }
+    else {
+        document.getElementById("mbtTOCc").style.display = 'block';
+    }
+    
+    
+    headlength = document.getElementById("post-toc").getElementsByTagName("h4").length;
+    //headlength = 5;
+    /*for (i = 0; i < headlength; i++) {
+        gethead = document.getElementById("post-toc").getElementsByTagName("h4")[i].textContent;
+        document.getElementById("post-toc").getElementsByTagName("h4")[i].setAttribute("id", "point" + i);
+        mbtTOC = document.getElementById("mbtTOCc");
+        tempTOC = "<li><a href='#point" + i + "'>" + gethead + "</a></li>";
+        mbtTOC += tempTOC;
+    }*/
+    
+    document.getElementById("mbtTOCc").textContent = ""
+    
+    for (i = 0; i < headlength; i++) {
+        gethead = document.getElementById("post-toc").getElementsByTagName("h4")[i].textContent;
+        document.getElementById("post-toc").getElementsByTagName("h4")[i].setAttribute("id", "point" + i);
+        //mbtTOC = document.getElementById("mbtTOCc");
+        
+        
+        
+        
+        var li = document.createElement('li');
+        li.setAttribute("id", "a-s_in_list"+i);
+
+        
+        var a = document.createElement('a');
+        a.setAttribute("href", "#point" + i);
+        a.textContent = gethead;
+        
+        
+        document.getElementById("mbtTOCc").appendChild(li);
+        document.getElementById("a-s_in_list"+i).appendChild(a);
+    }
+    
+}
+
 const button = document.getElementById('toc_butt');
 button.addEventListener('click', () => {
     
-    function mbtTOC() {
-        var mbtTOC = 0;
-        var i = 0;
-        var headlength = 0;
-        var gethead = 0;
-        var tempTOC = 0;
-        
-        headlength = document.getElementById("post-toc").getElementsByTagName("h4").length;
-        
-        /*for (i = 0; i < headlength; i++) {
-            gethead = document.getElementById("post-toc").getElementsByTagName("h4")[i].textContent;
-            document.getElementById("post-toc").getElementsByTagName("h4")[i].setAttribute("id", "point" + i);
-            mbtTOC = document.getElementById("mbtTOCc");
-            tempTOC = "<li><a href='#point" + i + "'>" + gethead + "</a></li>";
-            mbtTOC += tempTOC;
-        }*/
-        
-        for (i = 0; i < headlength; i++) {
-            gethead = document.getElementById("post-toc").getElementsByTagName("h4")[i].textContent;
-            document.getElementById("post-toc").getElementsByTagName("h4")[i].setAttribute("id", "point" + i);
-            //mbtTOC = document.getElementById("mbtTOCc");
-            var h6 = document.createElement('h6');
-            h6.textContent = "Point";
-            document.body.appendChild(h6);
-        }
-        
-    }
 
     /*function mbtToggle() {
         var mbt = document.getElementById('mbtTOCc');
@@ -38,5 +62,10 @@ button.addEventListener('click', () => {
         }
     }*/
 
+    
+    /*const h1 = document.createElement('h1');
+    h1.textContent = 'Hello World!';
+    document.body.appendChild(h1);*/
+    
     mbtTOC();
 });
