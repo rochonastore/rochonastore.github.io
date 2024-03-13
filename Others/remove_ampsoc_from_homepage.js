9ef601846857c86fded1ddfbdb73dde04e9d7384
Rochonastore <58441054+rochonastore@users.noreply.github.com>
2024-03-13 13:30:15 +0600

    Update remove_ampsoc_from_homepage.js

--- Others/remove_ampsoc_from_homepage.js
+++ Others/remove_ampsoc_from_homepage.js
@@ -1,9 +1,13 @@
 var ampsocs = document.getElementsByClassName("ampsoc");
-for (var i = 0; i < ampsocs.length; i++) {
-    ampsocs[i].style.display = "none";
+if(ampsocs.length > 2){
+    for (var i = 0; i < ampsocs.length; i++) {
+        ampsocs[i].style.display = "none";
+    }
 }
 
 var related_posts = document.getElementsByClassName("related-post-amp");
-for (var i = 0; i < related_posts.length; i++) {
-    related_posts[i].style.display = "none";
+if(related_posts.length > 1){
+    for (var i = 0; i < related_posts.length; i++) {
+        related_posts[i].style.display = "none";
+    }
 }
